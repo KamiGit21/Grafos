d50da639e9a903949bc03b4d10a58cc4af200bd7
KamiGit21 <kamila.jimenez@ucb.edu.bo>
2025-10-30 16:23:46 -0400

    arreglo merge

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,7 @@ import AsignacionRoutes from '../features/Asignacion/routes/routes.js';
 import SortsRoutes from '../features/Sorts/routes/routes.js';
 import NorthWestRoutes from '../features/NorthWest/routes/routes.js';
 import TreesRoutes from '../features/Trees/routes/routes.js'; // ← Agregar esta línea
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/Santi_branch_2.0
 
 const routes = [
   ...homeRoutes,
@@ -16,11 +13,7 @@ const routes = [
   ...AsignacionRoutes,
   ...SortsRoutes,
   ...NorthWestRoutes,
-<<<<<<< HEAD
-  ...TreesRoutes, // ← Agregar esta línea
-=======
   ...TreesRoutes,
->>>>>>> origin/Santi_branch_2.0
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
